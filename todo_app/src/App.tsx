8e907a49eb0c6cadf7b708afe7d5eff18377621a
worldyone <amanohikari142857@gmail.com>
2021-05-03 23:51:40 +0900

    追加ボタンの実装

--- todo_app/src/App.tsx
+++ todo_app/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, TextField } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase';
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{id:"", title:""}]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({id: doc.id, title: doc.data().title}))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: any) => {
+    db.collection("tasks").add({title: input});
+    setInput("");
+  }
+
+  return <div className="App">
+    <h1>Todo App by React/Firebase</h1>
+    <FormControl>
+      <TextField
+        InputLabelProps={{
+          shrink: true,
+        }}
+        label="New task ?"
+        value={input}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+      />
+    </FormControl>
+    <button disabled={!input} onClick={newTask}>
+      <AddToPhotosIcon />
+    </button>
+
+    {tasks.map((task) => (
+      <h3 key={task.id}>{task.title}</h3>
+    ))}
+  </div>
 }
 
 export default App;
--- todo_app/src/firebase.ts
+++ todo_app/src/firebase.ts
@@ -4,13 +4,20 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCNUysQz5DALgLiIc_dfpXuFCQ7axNOMLc",
+    // apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "udemy-todo-b7ad6.firebaseapp.com",
+    // authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: "https://udemy-todo-b7ad6.firebaseio.com",
+    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: "udemy-todo-b7ad6",
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: "udemy-todo-b7ad6.appspot.com",
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: "913344749301",
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: "1:913344749301:web:192f5694e8a1dc408aafbc"
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const db = firebaseApp.firestore();
